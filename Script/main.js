467e693acbcea2089f5debb8213d215033c2627d
Your Name <Zhuldyzkushkinbayeva@gmail.com>
2022-09-12 20:42:45 +0500

    add: add JS

--- Script/main.js
+++ Script/main.js
@@ -1,6 +1,23 @@
+"use strict";
 let mainBtn = document.querySelector("button");
 console.log(mainBtn);
 mainBtn.addEventListener("click", function () {
 	alert("lol");
 	console.log("lol");
 });
+let myname = "Aiesha";
+let surname = " Rocksan";
+
+console.log(myname + surname);
+typeof null;
+function Plus(a, b) {
+	a + b;
+}
+console.log(Plus(6, 4));
+let obj = {
+	name: "ali",
+	SayHello() {
+		console.log(obj.name);
+	},
+};
+obj.SayHello();
